a403bd215517f23c8d96b3fca5b67ef42012999d
Renato Moschetta <renatorock3@hotmail.com>
2020-07-29 16:30:54 -0300

    update class level 4

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,16 +1,20 @@
 import AppError from "@shared/errors/AppError";
 
+import FakeNotificationsRepository from "@modules/notifications/repositories/fakes/FakeNotificationsRepository";
 import FakeAppoitnmentsRepository from "../repositories/fakes/FakeAppointmentsRepository";
 import CreateAppointmentService from "./CreateAppointmentService";
 
 let fakeAppoitnmentsRepository: FakeAppoitnmentsRepository;
+let fakeNotificationsRepository: FakeNotificationsRepository;
 let createAppointment: CreateAppointmentService;
 
 describe("CreateAppointment", () => {
   beforeEach(() => {
     fakeAppoitnmentsRepository = new FakeAppoitnmentsRepository();
+    fakeNotificationsRepository = new FakeNotificationsRepository();
     createAppointment = new CreateAppointmentService(
-      fakeAppoitnmentsRepository
+      fakeAppoitnmentsRepository,
+      fakeNotificationsRepository
     );
   });
 
